82e5d0d891ea85676e82f9f6e5077739e2e8bfa7
unknown <not1337.doug@gmail.com>
2022-04-11 00:05:48 -0500

    first commit

---
+++ prjEulr.go
@@ -0,0 +1,37 @@
+package prjEulr
+
+import (
+
+	"context"
+	"time"
+
+	"go.temporal.io/sdk/activity"
+	"go.temporal.io/sdk/workflow"
+
+)
+
+func Workflow(ctx workflow.Context, name string) (string, error) {
+	ao := workflow.ActivityOption {
+		StartToCloesTimeout: 10 * time.Second,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	logger := workflow.GetLogger(ctx)
+	logger.Info("prjEulr wf started", "name", name)
+
+	var res string
+	err := workflow.ExecuteActivity(ctx, Activity, name).Get(ctx, &res)
+	if err != nil {
+		logger.Error("Activity failed.", "Error", err)
+		return "", err
+	}
+	logger.Info("prjEulr workflow completed.", "result", res)
+
+	return result, nil
+}
+
+func Activity(ctx context.Context, name string) (tring, error) {
+	logger := activity.GetLogger(ctx)
+	logger.Info("Activity", "name", name)
+	return "ahoy " + name + ".", nil
+}
